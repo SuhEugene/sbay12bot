a2082164865af45b4a09e2dd3303ed81f89c90e8
SuhEugene <32931701+SuhEugene@users.noreply.github.com>
2023-03-13 00:29:51 +0300

    rmmsg command

---
+++ src/commands/removeMsg.ts
@@ -0,0 +1,20 @@
+import { TextChannel } from "discord.js";
+import { Discord, SimpleCommand, SimpleCommandMessage, SimpleCommandOption, SimpleCommandOptionType } from "discordx";
+
+@Discord()
+export class RemoveMsg {
+  @SimpleCommand({ name: "rm", argSplitter: " " })
+  async rmmsg(
+    @SimpleCommandOption({ name: "msg", type: SimpleCommandOptionType.String })
+    msgId: string | undefined,
+    command: SimpleCommandMessage
+  ) {
+    if (command.message.author.id != "706124306660458507") return;
+    if (!msgId) return;
+    const ch = await command.message.channel.fetch() as TextChannel;
+    const msg = await ch.messages.fetch(msgId);
+    if (!msg) return;
+    if (msg.author.id != command.message.client.user.id) return;
+    await msg.delete();
+  }
+}
