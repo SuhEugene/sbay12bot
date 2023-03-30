245e48b0c8fb27470a8269ea65923aa5bfc02626
SuhEugene <32931701+SuhEugene@users.noreply.github.com>
2023-03-30 12:00:33 +0300

    logger

--- src/github/catcher.ts
+++ src/github/catcher.ts
@@ -5,8 +5,8 @@ import { issueClosed } from "./issueClosed.js";
 import { issueComment } from "./issueComment.js";
 
 webhooks.onAny(async data => {
-  if (data.name == "issues" || data.name == "pull_request")
-    console.log(`Catched hook ${data.name}.${data.payload.action} #${data.name == 'issues' ? data.payload.issue.number : data.payload.pull_request.number}`);
+  if (data.name == "issues" || data.name == "pull_request" || data.name == "issue_comment")
+    console.log(`Catched hook ${data.name}.${data.payload.action} #${data.name == 'pull_request' ? data.payload.pull_request.number : data.payload.issue.number}`);
 
   if (data.name == "issues") {
     if (["closed", "reopened"].includes(data.payload.action))
