cc0ab7e90abe3ed8ceedfb11e5152b983ba62f26
SuhEugene <32931701+SuhEugene@users.noreply.github.com>
2023-04-02 22:25:00 +0300

    Repochecker

--- src/github/repoCheck.ts
+++ src/github/repoCheck.ts
@@ -2,8 +2,51 @@ import { promises as fs } from "fs";
 import { shared } from "../shared.js";
 import path from "path";
 import { cwd } from "process";
+import { Octokit, RestEndpointMethodTypes } from "@octokit/rest";
+import simpleGit, { ResetMode } from "simple-git";
 
 const filePath = path.join(cwd(), "src", "data", "lastFetch.txt");
+const repoPath = path.join(cwd(), "repo");
+const git = simpleGit(repoPath);
+
+
+async function getSinceDate() {
+  let sinceDate = new Date(1680383635141);
+  try {
+    const got = await fs.readFile(filePath, "utf-8");
+    if (got) sinceDate = new Date(got);
+  } catch (e) {}
+
+  return sinceDate;
+}
+
+async function writeSinceDate(currentTime: Date) {
+  await fs.writeFile(filePath, currentTime.toISOString(), "utf-8");
+}
+
+async function getPRsToMerge(octo: Octokit, owner: string, repo: string, sinceDate: Date) {
+  const PRs: RestEndpointMethodTypes["pulls"]["list"]["response"]["data"] = [];
+
+  const prPaginator = octo.paginate.iterator(octo.pulls.list, {
+    owner, repo,
+    state: "closed",
+    sort: "updated",
+    direction: "desc",
+  });
+
+  let end = false;
+  for await (const page of prPaginator) {
+    for (const pr of page.data) {
+      if (new Date(pr.updated_at) < sinceDate)  { end = true; break; }
+      if (!pr.merged_at) continue;
+      PRs.push(pr);
+    }
+    if (end) break;
+  }
+
+  return PRs.reverse();
+}
+
 
 export async function checkRepo() {
   const octo = shared.octokit;
@@ -17,42 +60,47 @@ export async function checkRepo() {
   const [ getOwner, getRepo ] = process.env["GET_REPO"].split("/");
   const [ owner, repo ] = process.env["REPORT_REPO"].split("/");
   
-  let sinceDate = new Date(1680383635141);
-  try {
-    const got = await fs.readFile(filePath, "utf-8");
-    if (got) sinceDate = new Date(got);
-  } catch (e) {}
-
+  const currentTime = new Date();
+  const sinceDate = await getSinceDate();
   console.log("Checking repo "+process.env["GET_REPO"]+". Since: ", sinceDate);
 
-  const { data: mergedPullRequests } = await octo.rest.pulls.list({
-    owner: getOwner, repo: getRepo,
-    state: "closed",
-    sort: "updated",
-    direction: "desc",
-    since: sinceDate.toISOString()
-  });
-  
-
-  for (const pullRequest of mergedPullRequests) {
-    if (pullRequest.merged_at) {
-      try {
-        await octo.rest.pulls.create({
-          owner, repo,
-          title: "[MIRROR] "+pullRequest.title,
-          head: `${pullRequest.head.repo?.owner?.login}:${pullRequest.head.ref}`,
-          base: "dev220",
-          body:
-            (pullRequest.body || "Описание отсутствует") +
-            "\n<hr />\n\n*PR сгенерирован автоматически*"
-        });
-      } catch (error) {
-        console.error('Error creating pull request:', error);
-      }
-    }
+  const repoExists = await git.checkIsRepo();
+  if (!repoExists) {
+    await git.clone(`https://github.com/${owner}/${repo}.git`, repoPath);
   }
 
-  const currentTime = new Date();
-  await fs.writeFile(filePath, currentTime.toISOString(), "utf-8");
+  const prs = await getPRsToMerge(octo, getOwner, getRepo, sinceDate);
+
+  for (const pr of prs) {
+    const branchName = `bay12-pr-${pr.number}`;
+    await git.fetch("origin");
+    await git.checkout("dev220");
+    await git.pull("origin", "dev220");
+    await git.checkoutBranch(branchName, "dev220");
+
+    const patch = await octo.request(pr.patch_url);
+
+    try {
+      await git.applyPatch(patch.data);
+    } catch (e) {
+      await git.reset(ResetMode.HARD);
+    }
+
+    await git.add(".");
+    await git.raw("commit", "--allow-conflicts", "-m", `Apply patch for PR #${pr.number}`);
+    await git.push("origin", branchName);
+
+    await octo.pulls.create({
+      owner, repo,
+      head: branchName,
+      base: "dev220",
+      title: `[MIRROR] ${pr.title}`,
+      body:
+        `# Оригинальный PR: ${pr.base.repo.owner.login}/${pr.base.repo.name}#${pr.number}\n`+
+        pr.body
+    });
+  }
+
+  await writeSinceDate(currentTime);
   
 }
