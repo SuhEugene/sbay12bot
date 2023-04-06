8dc41f5ad5b08fdfea9b68ec82fbedbc30d10e2f
SuhEugene <32931701+SuhEugene@users.noreply.github.com>
2023-04-06 11:28:28 +0300

    Revert Test

--- src/github/catcher.ts
+++ src/github/catcher.ts
@@ -8,9 +8,6 @@ webhooks.onAny(async data => {
   if (data.name == "issues" || data.name == "pull_request" || data.name == "issue_comment")
     console.log(`Catched hook ${data.name}.${data.payload.action} #${data.name == 'pull_request' ? data.payload.pull_request.number : data.payload.issue.number}`);
 
-  if (data.name == "pull_request")
-    console.log("TEST DATA USER", data.payload.pull_request.user);
-
   if (data.name == "issues") {
     if (["closed", "reopened"].includes(data.payload.action))
       return await issueClosed(data as EmitterWebhookEvent<"issues.closed"> | EmitterWebhookEvent<"issues.reopened">);
