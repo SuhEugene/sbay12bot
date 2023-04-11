77dbec04212ea4ef9c3ed385658322665210ffc5
SuhEugene <32931701+SuhEugene@users.noreply.github.com>
2023-04-11 15:51:11 +0300

    Fails > succ

--- src/github/repoCheck.ts
+++ src/github/repoCheck.ts
@@ -131,19 +131,19 @@ export async function checkRepo() {
       const fails = (e.message as string).split("error: patch failed").length-1;
       const successes = (e.message as string).split("Applied patch to").length-1;
 
-      let scssc = 0; // additional successes for binaries;
-      const test = (e.message as string).matchAll(/warning: Cannot merge binary files: ([^(]+)\(ours vs. theirs\)/g);
-      for (const el of test) {
-        const path = el[1].trim();
-        console.log(`[PRMERGE] Checking out theirs binary ${path}...`);
-        await git.checkout(path, ["--theirs"]);
-        scssc++;
-      }
+      // let scssc = 0; // additional successes for binaries;
+      // const test = (e.message as string).matchAll(/warning: Cannot merge binary files: ([^(]+)\(ours vs. theirs\)/g);
+      // for (const el of test) {
+      //   const path = el[1].trim();
+      //   console.log(`[PRMERGE] Checking out theirs binary ${path}...`);
+      //   await git.checkout(path, ["--theirs"]);
+      //   scssc++;
+      // }
 
-      if (fails !== (successes+scssc) || !fails || !successes) {
+      if (fails > successes || !fails || !successes) {
         await git.reset(ResetMode.HARD, log);
         console.error("Patch couldn't be applied!\n\n", e);
-        console.error(`(Fails: ${fails}) != (Successes: ${successes})`);
+        console.error(`(Fails: ${fails}) > (Successes: ${successes})`);
         return;
       }
     }
