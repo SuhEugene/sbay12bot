e3c3c297c464670f222a4233083cc0786685eb83
Eugene Suharev <e.sukharev@ritm.media>
2026-01-29 17:26:15 +0300

    Sanitize all of the github PAT

--- src/utils/sanitOut.ts
+++ src/utils/sanitOut.ts
@@ -2,5 +2,6 @@ export function sanitOut(outString: string) {
   return outString
     .replaceAll(process.env["GITHUB_SECRET"]!, "[REDACTED GITHUB SECRET]")
     .replaceAll(process.env["BOT_TOKEN"]!, "[REDACTED BOT TOKEN]")
-    .replaceAll(process.env["GITHUB_TOKEN"]!, "[REDACTED GITHUB TOKEN]");
+    .replaceAll(process.env["GITHUB_TOKEN"]!, "[REDACTED GITHUB TOKEN]")
+    .replaceAll("/ghp_[a-zA-Z0-9]+", "[REDACTED GITHUB PAT]")
 }
