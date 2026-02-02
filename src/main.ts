159cbfe3eb826fe4be76518168bfc8e84b158b84
Eugene Suharev <e.sukharev@ritm.media>
2026-02-02 16:01:12 +0300

    Log milestone correctly

--- src/main.ts
+++ src/main.ts
@@ -96,7 +96,7 @@ bot.once("ready", async () => {
   console.log(` Report channel:    #${reportChannel.name} [${reportChannel.id}]`);
   console.log(` Mirror channel:    #${mirrorChannel.name} [${mirrorChannel.id}]`);
   console.log(` Changelog channel: #${changelogChannel.name} [${changelogChannel.id}]`);
-  console.log(` Report GitHub:     ${process.env["REPORT_REPO"]}:${process.env["BASE_BRANCH"]}${mstone && (', Milestone: ' + mstone)}`);
+  console.log(` Report GitHub:     ${process.env["REPORT_REPO"]}:${process.env["BASE_BRANCH"]}, Milestone: ${mstone ?? 'Not set'}`);
   console.log(` Fetch GitHub:      ${process.env["GET_REPO"]}`);
 
   console.log(` Allowed roles:`);
